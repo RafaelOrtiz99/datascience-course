087ebf8eb19c424fbc0a5483adda7f4da0662821
Rafael <rortizr@unbosque.edu.co>
2020-09-11 12:51:00 -0500

    JSON

--- Tema1/scripts/3.analisis-json.R
+++ Tema1/scripts/3.analisis-json.R
@@ -3,8 +3,27 @@
 #install.packages('curl')
 #library(curl)
 
+#FromSON = Cargar un archivo JSON
+#toJSON = Generar u archivo JSON desde un dataframe
+
 data.1 <- fromJSON('Recursos/r-course-master/data/tema1/students.json')
 data.2 <- fromJSON('Recursos/r-course-master/data/tema1/student-courses.json')
 
 url <- 'http://www.floatrates.com/daily/usd.json'
 currencies <- fromJSON(url)
+
+currencies.data <- currencies$all
+
+head(data.1, 3)
+head(data.1$Email)
+
+# Estudiantes 2,5 y 8
+data.1[c(2,5,8),]
+
+# Nombre y año
+data.1[,c(2,5)]
+
+head(data.2,3)
+
+data.2
+data.2[,c(2,3,6)]
