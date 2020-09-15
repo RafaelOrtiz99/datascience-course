96ea86959cf1393f6bfc97cbe2c164dc870f0e7a
RafaelOrtiz99 <rortizr@unbosque.edu.co>
2020-09-15 12:10:02 -0500

    duplicados

---
+++ Tema1/scripts/9-remove-duplicates.R
@@ -0,0 +1,14 @@
+family.salary = c(40000,60000,50000,80000,60000,70000,60000)
+family.size = c(4,3,2,2,3,4,3)
+family.car = c('lujo','compacto','utilitario','lujo','compacto','compacto','compacto')
+
+family <- data.frame(family.salary,family.size,family.car)
+
+#Eliminar duplicados
+family.unique <-unique(family)
+
+#Devuelve vector con booleanos, TRUE = duplicados
+duplicated(family)
+
+#Obtener duplicados
+family[duplicated(family),]
